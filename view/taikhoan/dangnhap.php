705e315ed173456129f2d363c569f06edd17ba60
tientbph38086e <tien.tran982004@gmail.com>
2023-11-19 15:28:50 +0700

    tiencommit1

---
+++ view/taikhoan/dangnhap.php
@@ -0,0 +1,20 @@
+<div class="container_titlect">
+        <div class="background_image">
+            <img src="image/banner2.png" alt="">
+        </div>
+        <div class="form">
+            <form action="index.php?act=dangnhap" method="post">
+                <h1>Đăng Nhập </h1>
+                <p>Hãy đăng nhập tài khoản để trải nghiệm những dịch vụ của chúng tôi!</p>
+                <div class="from_input">
+                    <input type="text" placeholder="Tên Tài Khoản" name="user">
+                    <input type="password" placeholder="Mật Khẩu" name="pass">
+                </div>
+                <input type="submit" value="Đăng nhập" name="dangnhap">
+                <div class="form_text">
+                    <p>Bạn chưa có tài khoản ư? <a href="index.php?act=dangky">Đăng ký</a></p>
+                </div>
+            </form> 
+        </div>
+        
+    </div>
