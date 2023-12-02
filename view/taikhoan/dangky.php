50e17c242bb4addb98e3692a992da4f4978c234c
Thai Doan Phong <thaidoanphong01122004@gmail.com>
2023-12-02 14:13:34 +0700

    okok

--- view/taikhoan/dangky.php
+++ view/taikhoan/dangky.php
@@ -1,28 +1,59 @@
 <div class="container_titlect">
-        <div class="background_image">
-            <img src="image/banner2.png" alt="">
-        </div>
-        <div class="form"> 
-            <form action="index.php?act=dangky" method="post"> 
-                <h1>Đăng Ký Tài Khoản</h1>
-                <p>Hãy đăng ký tài khoản để trải nghiệm những dịch vụ của chúng tôi!</p>
-                <div class="from_input">
-                    <input type="text" placeholder="Tên Tài Khoản" name="user">
-                    <input type="text" placeholder="Email" name="email">
-                    <input type="password" placeholder="Mật Khẩu" name="pass">
-                </div>
-                <input type="submit" value="Đăng Ký" name="dangky">
-                <h3 class="thongbao">
-            <?php        
-                if (isset($thongbao)&&($thongbao!="")) {
-                # code...
-                echo $thongbao;
+    <div class="background_image">
+        <img src="image/banner2.png" alt="">
+    </div>
+    <div class="form"> 
+        <form action="index.php?act=dangky" method="post" onsubmit="return validateForm()"> 
+            <h1>Đăng Ký Tài Khoản</h1>
+            <p>Hãy đăng ký tài khoản để trải nghiệm những dịch vụ của chúng tôi!</p>
+            <div class="from_input">
+                <input type="text" id="name" placeholder="Tên Tài Khoản" name="user" required>
+                <input type="text" id="email" placeholder="Email" name="email" required>
+                <input type="password" id="pass" placeholder="Mật Khẩu" name="pass" required>
+            </div>
+            <input type="submit" value="Đăng Ký" name="dangky">
+            <h3 class="thongbao">
+                <?php        
+                if (isset($thongbao) && $thongbao != "") {
+                    echo $thongbao;
                 }                       
-            ?>
-        </h3> 
-                <div class="form_text">
-                    <p>Bạn đã có tài khoản ư? <a href="index.php?act=dangnhap">Đăng Nhập</a></p>
-                </div>
-            </form> 
-        </div>  
+                ?>
+            </h3> 
+            <div class="form_text">
+                <p>Bạn đã có tài khoản ư? <a href="index.php?act=dangnhap">Đăng Nhập</a></p>
+            </div>
+        </form> 
+    </div>  
 </div>
+<script>
+    function validateForm() {
+        let emailInput = document.getElementById('email');
+        let regexEmail = /^\b[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b$/;
+        let isValidEmail = regexEmail.test(emailInput.value);
+        
+        if (emailInput.value === "" || emailInput.value === null) {
+            alert("Vui lòng nhập Email");
+            return false; // Prevent form submission
+        } else if (!isValidEmail) {
+            alert('Email không hợp lệ');
+            return false; // Prevent form submission
+        }
+        let passInput = document.getElementById('pass');
+        if (passInput.value === "" || passInput.value === null) {
+            alert("Vui lòng nhập mật khẩu");
+            return false; // Prevent form submission
+        } else if (passInput.value.length < 6) {
+            alert("Mật khẩu khoản phải có nhiều hơn 6 ký tự");
+            return false; // Prevent form submission
+        }
+        let nameInput = document.getElementById('name');
+        if (nameInput.value === "" || nameInput.value === null) {
+            alert("Vui lòng nhập tên tài khoản");
+            return false; // Prevent form submission
+        } else if (nameInput.value.length < 6) {
+            alert("Tên tài khoản phải có nhiều hơn 6 ký tự");
+            return false; // Prevent form submission
+        } 
+        return true; // Allow form submission
+    }
+</script>
--- view/taikhoan/edit_taikhoan.php
+++ view/taikhoan/edit_taikhoan.php
@@ -5,18 +5,18 @@
                             extract($_SESSION['user']);
                         }
                     ?>
-                       <form action="index.php?act=edit_taikhoan" method="post">
+                       <form action="index.php?act=edit_taikhoan" method="post" onsubmit="return validateForm()">
                             <div class="row">
                                 Email <br>
-                                <input type="email" name="email" value="<?=$email?>">
+                                <input type="email" name="email" value="<?=$email?>" id="email">
                             </div>
                             <div class="row">
                                 Tên Đăng Nhập <br>
-                                <input type="text" name="user" value="<?=$user?>">
+                                <input type="text" name="user" value="<?=$user?>" id="name">
                             </div>  
                             <div class="row">
                                 Mật Khẩu <br>
-                                <input type="password" name="pass" value="<?=$pass?>">
+                                <input type="password" name="pass" value="<?=$pass?>" id="pass">
                             </div>
                             <div class="row">
                                 Địa chỉ<br>
@@ -24,7 +24,7 @@
                             </div>
                             <div class="row">
                                 Điện Thoại <br>
-                                <input type="text" name="tel" value="<?=$tel?>">
+                                <input type="text" name="tel" value="<?=$tel?>" id="sdt">
                             </div>
                             <div class="row">
                                 <input type="hidden" name="id" value="<?=$id?>">
@@ -42,3 +42,36 @@
                             ?>
                         </h2>
                 </div>
+
+                <script>
+    function validateForm() {
+        let emailInput = document.getElementById('email');
+        let regexEmail = /^\b[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b$/;
+        let isValidEmail = regexEmail.test(emailInput.value);
+        
+        if (emailInput.value === "" || emailInput.value === null) {
+            alert("Vui lòng nhập Email");
+            return false; // Prevent form submission
+        } else if (!isValidEmail) {
+            alert('Email không hợp lệ');
+            return false; // Prevent form submission
+        }
+        let passInput = document.getElementById('pass');
+        if (passInput.value === "" || passInput.value === null) {
+            alert("Vui lòng nhập mật khẩu");
+            return false; // Prevent form submission
+        } else if (passInput.value.length < 6) {
+            alert("Mật khẩu khoản phải có nhiều hơn 6 ký tự");
+            return false; // Prevent form submission
+        }
+        let nameInput = document.getElementById('name');
+        if (nameInput.value === "" || nameInput.value === null) {
+            alert("Vui lòng nhập tên tài khoản");
+            return false; // Prevent form submission
+        } else if (nameInput.value.length < 6) {
+            alert("Tên tài khoản phải có nhiều hơn 6 ký tự");
+            return false; // Prevent form submission
+        } 
+        return true; // Allow form submission
+    }
+</script>
